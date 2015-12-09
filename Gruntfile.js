c9124be32fa2576425c3748ed1dc9e5f1857b612
balessan <benoit.alessandroni@gmail.com>
2015-12-09 20:52:18 +0100

    Adding everything to be able to watch the changes and add automatic compiling
    Conflicts:
    	Gruntfile.js
    	package.json

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      js: {
+        files: ['./**.js'],
+        tasks: ['browserify']
+      }
+    },
+    browserify: {
+      js: {
+        src: './ldpstore.js',
+        dest: './mystore.js'
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-browserify')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.registerTask('default', ['watch', 'browserify'])
+}
